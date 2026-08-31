d5193fe0d8a96e81a21bceb7740599698f3bdbb8
Wendler Vinicius Silva Rodrigues <wendlervinicius9@gmail.com>
2026-08-31 11:25:24 -0300

    fear: implementa reinicio da aplicacao

--- app.js
+++ app.js
@@ -60,13 +60,35 @@ function sortear() {
     }
 
     const resultado = document.getElementById('resultado');
-    resultado.innerHTML = 
-'<label class = "texto_paragrafo">' +
- `Números sorteados: ${sorteados.join(', ')}`} + '</label>';
+    resultado.innerHTML =
+        '<label class = "texto__paragrafo">' +
+        `Números sorteados: ${sorteados.join(', ')}`
+        + '</label>';
+
+    //Controlar o botão Reiniciar
+
+    const botaoReiniciar = document.getElementById('btn-reiniciar');
+    botaoReiniciar.disabled = false;
+    botaoReiniciar.classList.remove('container__botao-desabilitado');
+    botaoReiniciar.classList.add('container__botao');
+
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '<label class = "texto__paragrafo">' + 'Números sorteados: nenhum até agora' + '</label>';
+
+    const botaoReiniciar = document.getElementById('btn-reiniciar');
+    botaoReiniciar.disabled = true;
+    botaoReiniciar.classList.remove('container__botao');
+    botaoReiniciar.classList.add('container__botao-desabilitado');
+    document.getElementById('quantidade').focus();
+}
 
 
 
 
-
-
- 
